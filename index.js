993ad4009976c31eab5964bd9b466ffe4852ec8a
DomiingosS <125277788+DomiingosS@users.noreply.github.com>
2024-02-20 15:05:25 +0000

    Criação do endpoint `/oi`

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ const app = express()
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
-
+app.get ("/oi", function (req, res){
+    res.send ("A aula foi muito bem explicada!")
+})
 app.listen(3000)
